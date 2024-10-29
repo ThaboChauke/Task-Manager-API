6319aa1adb71c896cd950d54a98e3ad7c9b4b041
ThaboChauke <chaukethabo16@gmail.com>
2024-10-29 22:46:06 +0200

    adding routes

--- main.py
+++ main.py
@@ -6,3 +6,26 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.post("/register")
+def register():
+    pass
+
+@app.post("/login")
+def login():
+    pass
+
+@app.post("/tasks")
+def create_task():
+    pass
+
+@app.get("/tasks")
+def get_tasks():
+    pass
+
+@app.put("/tasks")
+def update_task():
+    pass
+
+@app.delete("/tasks")
+def delete_task():
+    pass
