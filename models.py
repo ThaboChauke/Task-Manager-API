63fc11a3306022325305e1fc87cab081a01fd7eb
ThaboChauke <chaukethabo16@gmail.com>
2024-11-03 11:58:56 +0200

    task models

--- models.py
+++ models.py
@@ -11,3 +11,17 @@ class Users(db.Model):
 
     def __repr__(self):
         return '<Name %r>' % self.name
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    status = db.Column(db.String(20), default="Pending")
+    priority = db.Column(db.String(20), default="Medium")
+    due_date = db.Column(db.DateTime, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
+
+    def __repr__(self):
+        return f"<Task {self.title}>"
